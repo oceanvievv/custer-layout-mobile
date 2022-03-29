836b8606267b52a6f9c7a8bedf60e51e279ae087
oceanvievv <oceanvievv@gmail.com>
2022-03-29 10:16:42 +0500

    header wip | search field

---
+++ js/handleSearchIconClick.js
@@ -0,0 +1,29 @@
+const burgerDef = document.getElementById("header_burger_default");
+const burgerExp = document.getElementById("header_burger_expanded");
+const logo = document.getElementById("header_logo");
+const searchIcon = document.getElementById("menu_search");
+const cartIcon = document.getElementById("menu_cart");
+
+const elements = [
+    burgerDef,
+    burgerExp,
+    logo,
+    searchIcon,
+    cartIcon
+];
+
+function hideHeaderElements() {
+    for (let el of elements) {
+        el.classList.toggle("header-elements-hidden")
+    }
+    
+    const header = document.getElementById("header-mobile");
+    header.classList.remove("header-mobile");
+    header.classList.add("header-mobile-with-search");
+}
+
+function showHeaderElements() {
+    for (let el of elements) {
+        el.classList.toggle("header-elements-hidden")
+    }
+}
